64c952bf593bb816ad56d05173236990500f52c2
yukeshram10 <yukeshram1094@gmail.com>
2022-07-15 22:05:33 +0530

    message

---
+++ src/org/tash/Education.java
@@ -0,0 +1,16 @@
+package org.tash;
+
+public class Education {
+	public void ug(String a) {
+		System.out.println("Program Type:" + a);
+	}
+	public void pg(String b) {
+		System.out.println("Program Type:" + b);
+	}
+	public static void main(String[] args) {
+		Education c = new Education();
+		c.ug("Under Graduate");
+		c.pg("Post Graduate");
+	}
+
+}
